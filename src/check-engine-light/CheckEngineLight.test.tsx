ceb07ffb923c1cb8994fdd146de76651d9c9b96c
Kyle Stoltzfus <computerwhiz33@gmail.com>
2021-08-31 21:21:31 -0400

    Added tests for CheckEngineLight module

---
+++ src/check-engine-light/CheckEngineLight.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import CheckEngineLight  from './CheckEngineLight';
+
+test('renders check engine light', () => {
+  render(<CheckEngineLight />);
+  const checkEngineLightElement = screen.getByAltText('checkEngineLightIcon');
+  expect(checkEngineLightElement).toBeInTheDocument();
+});
